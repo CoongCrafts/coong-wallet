0b276b964f82fc89d883d4c6d09be37c992a7d29
Thang X. Vu <zthangxv@gmail.com>
2023-02-02 10:30:13 +0700

    Add accounts list & basic action for Request Access action

--- packages/ui/src/components/pages/Request/RequestAccess.tsx
+++ packages/ui/src/components/pages/Request/RequestAccess.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { state } from '@coong/base';
 import { RequestAppRequestAccess, WalletRequestWithResolver } from '@coong/base/types';
 import { Button, styled } from '@mui/material';
+import NewAccountButton from 'components/shared/NewAccountButton';
+import SelectableAccountCard from 'components/shared/accounts/SelectableAccountCard';
+import useAccounts from 'hooks/accounts/useAccounts';
+import { accountsActions } from 'redux/slices/accounts';
 import { RootState } from 'redux/store';
 import { Props } from 'types';
 
@@ -12,11 +16,16 @@ interface RequestAccessProps extends Props {
 }
 
 const RequestAccess: FC<RequestAccessProps> = ({ className = '', message }) => {
-  const { seedReady } = useSelector((state: RootState) => state.app);
+  const {
+    app: { seedReady },
+    accounts: { selectedAccounts },
+  } = useSelector((state: RootState) => state);
+  const accounts = useAccounts();
+  const dispatch = useDispatch();
 
   const acceptAccess = async () => {
     try {
-      state.approveRequestAccess([]);
+      state.approveRequestAccess(selectedAccounts.map((one) => one.address));
     } catch (e: any) {
       toast.error(e.message);
     }
@@ -30,10 +39,19 @@ const RequestAccess: FC<RequestAccessProps> = ({ className = '', message }) => {
     console.log('Trigger setup wallet flow!');
   };
 
+  const doSelectAll = () => {
+    dispatch(accountsActions.addSelectedAccounts(accounts));
+  };
+
+  const doDeselectAll = () => {
+    dispatch(accountsActions.removeSelectedAccounts(accounts));
+  };
+
   const { origin, request } = message;
 
   const requestBody = request.body as RequestAppRequestAccess;
 
+  // TODO: Optimize create new account behavior
   return (
     <div className={className}>
       <h2 className='text-center'>Wallet Access Request</h2>
@@ -44,12 +62,53 @@ const RequestAccess: FC<RequestAccessProps> = ({ className = '', message }) => {
 
       {seedReady ? (
         <div>
-          <p>Only connect if you trust the application</p>
-          <div className='button-groups'>
-            <Button variant='outlined' color='warning' onClick={rejectAccess}>
-              Cancel
-            </Button>
-            <Button onClick={acceptAccess}>Connect</Button>
+          <h3>Select the accounts you'd like to connect</h3>
+
+          <div className='accounts-selection'>
+            <div className='accounts-selection--top'>
+              <div></div>
+              <div>
+                {accounts.length > selectedAccounts.length && (
+                  <Button size='small' variant='outlined' onClick={doSelectAll}>
+                    Select all
+                  </Button>
+                )}
+                {accounts.length == selectedAccounts.length && (
+                  <Button size='small' variant='outlined' onClick={doDeselectAll}>
+                    Deselect all
+                  </Button>
+                )}
+              </div>
+            </div>
+            <div className='accounts-selection--list'>
+              {accounts.map((account) => (
+                <SelectableAccountCard key={account.address} account={account} />
+              ))}
+            </div>
+            <div className='accounts-selection--bottom'>
+              <div>
+                {selectedAccounts.length ? (
+                  <span>
+                    <strong>{selectedAccounts.length}</strong> account(s) selected
+                  </span>
+                ) : (
+                  <span>No accounts selected</span>
+                )}
+              </div>
+              <NewAccountButton />
+            </div>
+          </div>
+
+          <div className='actions'>
+            <p>Only connect if you trust the application</p>
+            <div className='button-groups'>
+              <Button size='large' variant='text' color='warning' onClick={rejectAccess}>
+                Cancel
+              </Button>
+              <Button size='large' fullWidth onClick={acceptAccess}>
+                Connect
+              </Button>
+            </div>
           </div>
         </div>
       ) : (
@@ -73,11 +132,32 @@ export default styled(RequestAccess)`
   }
 
   .button-groups {
-    text-align: center;
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
 
-    button + button {
-      margin-left: 0.5rem;
-      width: 170px;
+  .accounts-selection {
+    margin-bottom: 1rem;
+
+    &--list {
+      max-height: 392px;
+      overflow-y: auto;
+    }
+
+    &--top,
+    &--bottom {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &--top {
+      margin-bottom: 0.5rem;
+    }
+
+    &--bottom {
+      margin-top: 0.5rem;
     }
   }
 `;
---
+++ packages/ui/src/components/shared/accounts/SelectableAccountCard.tsx
@@ -0,0 +1,100 @@
+import { Identicon } from '@polkadot/react-identicon';
+import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AccountInfo } from '@coong/keyring/types';
+import { Check } from '@mui/icons-material';
+import { styled } from '@mui/material';
+import CopyAddressTooltip from 'components/shared/CopyAddressTooltip';
+import { accountsActions } from 'redux/slices/accounts';
+import { RootState } from 'redux/store';
+import { Props } from 'types';
+import { shortenAddress } from 'utils/string';
+
+interface SelectableAccountCardProps extends Props {
+  account: AccountInfo;
+}
+
+const SelectableAccountCard: FC<SelectableAccountCardProps> = ({ className = '', account }) => {
+  const { selectedAccounts } = useSelector((state: RootState) => state.accounts);
+  const dispatch = useDispatch();
+
+  const { address, name } = account;
+
+  const selected = selectedAccounts.map((one) => one.address).includes(account.address);
+  const doSelect = () => {
+    if (selected) {
+      dispatch(accountsActions.removeSelectedAccounts([account]));
+    } else {
+      dispatch(accountsActions.addSelectedAccounts([account]));
+    }
+  };
+
+  return (
+    <div className={`${className} selectable-account-card ${selected ? 'selected' : ''}`} onClick={doSelect}>
+      <div className='selectable-account-card__left'>
+        <div className='selectable-account-card__icon'>
+          <CopyAddressTooltip address={address} name={name}>
+            <Identicon value={address} size={24} theme='polkadot' />
+          </CopyAddressTooltip>
+        </div>
+        <div className='selectable-account-card__info'>
+          <div className='selectable-account-card__name'>{name}</div>
+          <div className='selectable-account-card__address'>({shortenAddress(address)})</div>
+        </div>
+      </div>
+      <div className='selectable-account-card__right'>{selected && <Check />}</div>
+    </div>
+  );
+};
+
+export default styled(SelectableAccountCard)`
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.5rem;
+  margin-bottom: 0.5rem;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &:hover {
+    background-color: rgba(104, 217, 96, 0.2);
+  }
+
+  &.selected {
+    background-color: rgba(104, 217, 96, 0.5);
+  }
+
+  .selectable-account-card {
+    &__left {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+
+    &__icon,
+    &__right {
+      font-size: 0;
+    }
+
+    &__info {
+      display: flex;
+      align-items: center;
+      font-size: 1rem !important;
+      gap: 0.2rem;
+    }
+
+    &__name {
+      font-weight: 600;
+    }
+
+    &__address {
+      font-size: 0.8rem;
+    }
+  }
+`;
