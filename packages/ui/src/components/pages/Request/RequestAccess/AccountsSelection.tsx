923f79e17b902c9b85db47ac94e992875dc502d2
Thang X. Vu <zthangxv@gmail.com>
2023-02-04 15:44:10 +0700

    Add search box for request access view

--- packages/ui/src/components/pages/Request/RequestAccess/AccountsSelection.tsx
+++ packages/ui/src/components/pages/Request/RequestAccess/AccountsSelection.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, styled } from '@mui/material';
+import { AccountInfo } from '@coong/keyring/types';
+import { Search } from '@mui/icons-material';
+import { Button, InputAdornment, styled, TextField } from '@mui/material';
 import NewAccountButton from 'components/shared/NewAccountButton';
 import SelectableAccountCard from 'components/shared/accounts/SelectableAccountCard';
 import useAccounts from 'hooks/accounts/useAccounts';
@@ -9,9 +11,15 @@ import { RootState } from 'redux/store';
 import { Props } from 'types';
 
 const AccountsSelection: FC<Props> = ({ className }) => {
-  const { selectedAccounts } = useSelector((state: RootState) => state.accounts);
   const accounts = useAccounts();
   const dispatch = useDispatch();
+  const { selectedAccounts } = useSelector((state: RootState) => state.accounts);
+  const [displayAccounts, setDisplayAccounts] = useState<AccountInfo[]>([]);
+  const [query, setQuery] = useState<string>('');
+
+  useEffect(() => {
+    setDisplayAccounts(accounts.filter((one) => one.name && one.name.toLowerCase().includes(query.toLowerCase())));
+  }, [accounts, query]);
 
   const doSelectAll = () => {
     dispatch(accountsActions.addSelectedAccounts(accounts));
@@ -24,7 +32,24 @@ const AccountsSelection: FC<Props> = ({ className }) => {
   return (
     <div className={`${className} accounts-selection`}>
       <div className='accounts-selection--top'>
-        <div></div>
+        <div>
+          <TextField
+            hiddenLabel
+            size='small'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <Search fontSize='small' />
+                </InputAdornment>
+              ),
+              className: 'pl-2 h-[30px] text-[0.8125rem]',
+            }}
+            placeholder='Search by name'
+            onChange={(event) => {
+              setQuery(event.target.value);
+            }}
+          />
+        </div>
         <div>
           {accounts.length > selectedAccounts.length && (
             <Button size='small' variant='outlined' onClick={doSelectAll}>
@@ -39,9 +64,14 @@ const AccountsSelection: FC<Props> = ({ className }) => {
         </div>
       </div>
       <div className='accounts-selection--list'>
-        {accounts.map((account) => (
+        {displayAccounts.map((account) => (
           <SelectableAccountCard key={account.address} account={account} />
         ))}
+        {displayAccounts.length === 0 && (
+          <p className='text-gray-500'>
+            No accounts meet search query: <strong>{query}</strong>
+          </p>
+        )}
       </div>
       <div className='accounts-selection--bottom'>
         <div>
