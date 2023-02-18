9b3ae2130916459661aa6bd1bedbd8e276b5dd6d
Thang X. Vu <zthangxv@gmail.com>
2023-02-18 17:54:30 +0700

    Update search box for xxs size

--- packages/ui/src/components/shared/accounts/SearchBox.tsx
+++ packages/ui/src/components/shared/accounts/SearchBox.tsx
@@ -1,33 +1,58 @@
-import { FC } from 'react';
-import { Search } from '@mui/icons-material';
-import { InputAdornment, TextField } from '@mui/material';
+import { FC, useEffect, useState } from 'react';
+import { Close, Search } from '@mui/icons-material';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import clsx from 'clsx';
 import { Props } from 'types';
 
 interface SearchBoxProps extends Props {
   onChange: (query: string) => void;
-  placeholder?: string;
+  label?: string;
+  autoFocus?: boolean;
+  size?: 'xxs' | 'xs';
 }
 
-const SearchBox: FC<SearchBoxProps> = ({ className = '', onChange, placeholder = 'Search by name' }) => {
+const SearchBox: FC<SearchBoxProps> = ({
+  className = '',
+  onChange,
+  label = 'Search by name',
+  autoFocus = false,
+  size = 'xs',
+}) => {
+  const [query, setQuery] = useState<string>('');
+
+  useEffect(() => {
+    onChange(query);
+  }, [query]);
+
+  const doClearSearchQuery = () => {
+    setQuery('');
+  };
+
+  const xxs = size === 'xxs';
+
   return (
-    <div className={className}>
-      <TextField
-        hiddenLabel
-        size='small'
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position='start'>
-              <Search fontSize='small' />
-            </InputAdornment>
-          ),
-          className: 'pl-2 h-[30px] text-[0.8125rem]',
-        }}
-        placeholder={placeholder}
-        onChange={(event) => {
-          onChange(event.target.value);
-        }}
-      />
-    </div>
+    <TextField
+      size='small'
+      value={query}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position='end'>
+            <IconButton edge='end' onClick={doClearSearchQuery}>
+              {query ? <Close /> : <Search />}
+            </IconButton>
+          </InputAdornment>
+        ),
+        className: clsx({ 'h-[30px] text-[0.8125rem]': size === 'xxs' }),
+      }}
+      label={xxs ? '' : label}
+      hiddenLabel={xxs}
+      placeholder={xxs ? label : ''}
+      className={className}
+      onChange={(event) => {
+        setQuery(event.target.value);
+      }}
+      autoFocus={autoFocus}
+    />
   );
 };
 export default SearchBox;
