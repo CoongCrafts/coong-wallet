55efed3caa627089ac6ee72440b6e7770ad3018e
Thang X. Vu <zthangxv@gmail.com>
2023-02-19 15:14:18 +0700

    Update welcome page style

--- packages/ui/src/components/pages/Welcome.tsx
+++ packages/ui/src/components/pages/Welcome.tsx
@@ -16,24 +16,24 @@ const Welcome: React.FC<Props> = ({ className = '' }: Props) => {
   };
 
   return (
-    <div className={`${className} my-16 mx-auto text-center`}>
+    <div className={`${className} mt-8 mb-16 mx-auto text-center`}>
       <div className='welcome'>
-        <h2>Welcome to Coong</h2>
-        <p>
+        <h1>Welcome to Coong</h1>
+        <p className='text-2xl'>
           A multichain crypto wallet
           <br />
           for <strong>Polkadot & Kusama</strong> ecosystem
         </p>
       </div>
       <div className='mt-8'>
-        <h5 className='mb-4'>Set up your Coong wallet now</h5>
+        <h4 className='mb-4'>Set up your Coong wallet now</h4>
 
         <div className='flex flex-col gap-4 items-center'>
           <Button size='large' className='min-w-[270px]' onClick={doCreateNewWallet}>
             Create new wallet
           </Button>
           <Button size='large' className='min-w-[270px]' variant='outlined' onClick={doRestoreWallet} disabled={true}>
-            Restore existing wallet
+            <span className='pr-1'>Restore existing wallet</span> <small>(Coming soon)</small>
           </Button>
         </div>
       </div>
