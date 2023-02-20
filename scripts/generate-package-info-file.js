62a5791638f35f3d9a4127f6d34bcd411d476a65
Thang X. Vu <zthangxv@gmail.com>
2023-02-20 17:26:27 +0700

    Add generate-package-info-file script

---
+++ scripts/generate-package-info-file.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const currentDir = process.cwd();
+const packageJson = JSON.parse(fs.readFileSync(path.join(currentDir, 'package.json'), 'utf8'));
+
+const { name, version } = packageJson;
+
+fs.writeFileSync(
+  path.join(currentDir, 'src/packageInfo.ts'),
+  `export const packageInfo = { name: '${name}', version: '${version}' };\n`,
+);
