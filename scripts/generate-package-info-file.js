909f4c83b86706cb085881eba83c5075c2d9fb64
Thang X. Vu <thang@coongwallet.io>
2023-02-27 15:32:37 +0700

    Generate packageInfo into dist

--- scripts/generate-package-info-file.js
+++ scripts/generate-package-info-file.js
@@ -7,8 +7,7 @@ const packageJson = JSON.parse(fs.readFileSync(path.join(currentDir, 'package.js
 const { name, version } = packageJson;
 
 const fileHeader = `// THIS FILE IS AUTO-GENERATED, DO NOT EDIT!\n`;
+const fileContent = `${fileHeader}\nexport const packageInfo = { name: '${name}', version: '${version}' };\n`;
 
-fs.writeFileSync(
-  path.join(currentDir, 'src/packageInfo.ts'),
-  `${fileHeader}\nexport const packageInfo = { name: '${name}', version: '${version}' };\n`,
-);
+fs.writeFileSync(path.join(currentDir, 'src/packageInfo.ts'), fileContent);
+fs.writeFileSync(path.join(currentDir, 'dist/packageInfo.js'), fileContent);
