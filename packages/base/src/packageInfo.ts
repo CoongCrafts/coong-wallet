71302713706fede2a7756b256a658d0411681213
Thang X. Vu <thang@coongwallet.io>
2023-02-27 15:10:01 +0700

    Update packageInfo files

--- packages/base/src/packageInfo.ts
+++ packages/base/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
 
-export const packageInfo = { name: '@coong/base', version: '0.0.12' };
+export const packageInfo = { name: '@coong/base', version: '0.0.13' };
