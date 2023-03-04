bc32400f91db3be179820fc8be6ab0b449a28247
Thang X. Vu <thang@coongwallet.io>
2023-03-04 08:39:16 +0700

    Remove unused files

--- packages/utils/src/assert.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function assert(condition: unknown, message?: string): void;
-/**
- * Throw out error if condition is undefined, false, null, '' or 0
- *
- * @param condition
- * @param message
- */
-export declare function assertFalse(condition: unknown, message?: string): void;
