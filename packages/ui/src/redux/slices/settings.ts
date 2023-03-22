f087cd35c340b2747b344aca931369972d65588f
1cedrus <aowiej23s@gmail.com>
2023-03-22 12:36:44 +0700

    refactor: rename settingsSlice #1

--- packages/ui/src/redux/slices/settings.ts
+++ packages/ui/src/redux/slices/settings.ts
@@ -1,28 +1,28 @@
 import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
 import { Language, ThemeMode } from 'types';
 
-export interface SettingState {
+export interface SettingsState {
   themeMode: ThemeMode;
   language: Language;
 }
 
-const initialState: SettingState = {
+const initialState: SettingsState = {
   themeMode: ThemeMode.System,
   language: Language.English,
 };
 
-const settingSlice = createSlice({
+const settingsSlice = createSlice({
   name: 'settings',
   initialState,
   reducers: {
-    switchThemeMode: (state: Draft<SettingState>, action: PayloadAction<ThemeMode>) => {
+    switchThemeMode: (state: Draft<SettingsState>, action: PayloadAction<ThemeMode>) => {
       state.themeMode = action.payload;
     },
-    switchLanguage: (state: Draft<SettingState>, action: PayloadAction<Language>) => {
+    switchLanguage: (state: Draft<SettingsState>, action: PayloadAction<Language>) => {
       state.language = action.payload;
     },
   },
 });
 
-export const settingActions = settingSlice.actions;
-export default settingSlice;
+export const settingActions = settingsSlice.actions;
+export default settingsSlice;
