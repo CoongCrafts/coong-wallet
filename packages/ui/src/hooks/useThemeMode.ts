a42d6a74376ad14ff67ce5ba05108a135e2a2024
Thang X. Vu <thang@coongwallet.io>
2023-04-20 17:22:55 +0700

    fix: system theme does not update on changing browser/os theme

--- packages/ui/src/hooks/useThemeMode.ts
+++ packages/ui/src/hooks/useThemeMode.ts
@@ -1,19 +1,20 @@
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { useMedia } from 'react-use';
 import { RootState } from 'redux/store';
 import { ThemeMode } from 'types';
 
 export default function useThemeMode() {
+  const systemPrefersDarkMode = useMedia('(prefers-color-scheme: dark)');
   const { themeMode: rawThemeMode } = useSelector((state: RootState) => state.settings);
 
   const themeMode = useMemo(() => {
     if (rawThemeMode === ThemeMode.System) {
-      const isSystemPrefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      return isSystemPrefersDarkMode ? ThemeMode.Dark : ThemeMode.Light;
+      return systemPrefersDarkMode ? ThemeMode.Dark : ThemeMode.Light;
     }
 
     return rawThemeMode;
-  }, [rawThemeMode]);
+  }, [rawThemeMode, systemPrefersDarkMode]);
 
   const dark = themeMode === ThemeMode.Dark;
   const light = themeMode === ThemeMode.Light;
