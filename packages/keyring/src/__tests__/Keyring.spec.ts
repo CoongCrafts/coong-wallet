e909d05a9994f6071f6a9adac8ac306e6d27ce4c
1cedrus <aowiej23s@gmail.com>
2023-04-27 20:23:26 +0700

    feat(keyring): rename account (#38)

--- packages/keyring/src/__tests__/Keyring.spec.ts
+++ packages/keyring/src/__tests__/Keyring.spec.ts
@@ -361,3 +361,37 @@ describe('removeAccount', () => {
     );
   });
 });
+
+describe('renameAccount', () => {
+  it('should throw an error when the name already exists', async () => {
+    const keyring = await initializeNewKeyring();
+    const { address } = await keyring.createNewAccount('test-account', PASSWORD);
+
+    await expect(keyring.renameAccount(address, 'test-account')).rejects.toThrowError(
+      new CoongError(ErrorCode.AccountNameUsed),
+    );
+  });
+
+  it('should run saveAccountMeta function', async () => {
+    const keyring = await initializeNewKeyring();
+    const { address } = await keyring.createNewAccount('test-account', PASSWORD);
+    const renameAccountSpy = vi.spyOn(Keyring.prototype, 'renameAccount');
+
+    await keyring.renameAccount(address, 'valid-name');
+
+    expect(renameAccountSpy).toBeCalled();
+  });
+
+  it('should able to find the account after renaming it', async () => {
+    const keyring = await initializeNewKeyring();
+    const { address } = await keyring.createNewAccount('test-account', PASSWORD);
+    const renameAccountSpy = vi.spyOn(Keyring.prototype, 'renameAccount');
+
+    await keyring.renameAccount(address, 'valid-name');
+
+    await expect(keyring.getAccountByName('valid-name')).resolves;
+    await expect(keyring.getAccountByName('test-account')).rejects.toThrowError(
+      new CoongError(ErrorCode.AccountNotFound),
+    );
+  });
+});
