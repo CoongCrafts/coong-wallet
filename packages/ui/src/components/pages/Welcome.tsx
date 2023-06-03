3941809bad38edc0e5e7a3c7692fbb69ac278002
Thang X. Vu <thang@coongwallet.io>
2023-06-03 15:01:22 +0700

    feat: add forget password button (#51)

--- packages/ui/src/components/pages/Welcome.tsx
+++ packages/ui/src/components/pages/Welcome.tsx
@@ -24,13 +24,13 @@ const Welcome: React.FC<WelcomeProps> = ({ className = '', onCreateNewWallet, on
   return (
     <div className={`${className} mt-8 mb-16 mx-auto text-center`}>
       <div className='welcome'>
-        <h1>{t<string>('Welcome to Coong')}</h1>
+        <h1>{t<string>('Welcome to Coong Wallet')}</h1>
         <p className='text-2xl'>
           <Trans>A multichain crypto wallet for Polkadot & Kusama ecosystem</Trans>
         </p>
       </div>
       <div className='mt-8'>
-        <h4 className='mb-4'>{t<string>('Set up your Coong wallet now')}</h4>
+        <h4 className='mb-4'>{t<string>('Set up your Coong Wallet now')}</h4>
 
         <div className='flex flex-col gap-4 items-center'>
           <Button size='large' className='min-w-[270px]' onClick={doCreateNewWallet}>
---
+++ packages/ui/src/components/shared/buttons/ForgotPasswordButton.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { useBoolean } from 'react-use';
+import { Box, Button, Dialog, DialogContent, DialogContentText, TextField } from '@mui/material';
+import DialogTitle from 'components/shared/DialogTitle';
+import useDialog from 'hooks/useDialog';
+import { useWalletState } from 'providers/WalletStateProvider';
+import { appActions } from 'redux/slices/app';
+
+const CONFIRMATION_WORDS = 'Reset wallet';
+
+export default function ForgotPasswordButton() {
+  const { t } = useTranslation();
+  const { open, doOpen, doClose } = useDialog();
+  const [confirmation, setConfirmation] = useState<string>('');
+  const [loading, setLoading] = useBoolean(false);
+  const { keyring } = useWalletState();
+  const dispatch = useDispatch();
+
+  const handleClose = () => doClose();
+
+  const doResetWallet = async () => {
+    if (confirmation !== CONFIRMATION_WORDS) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setTimeout(async () => {
+        await keyring.reset();
+        dispatch(appActions.seedReady(false));
+      }, 200);
+    } catch (e: any) {
+      setLoading(false);
+      toast.error(t<string>(e.message));
+    }
+  };
+
+  return (
+    <>
+      <Button variant='text' color='gray' onClick={doOpen}>
+        Forgot your password?
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle onClose={handleClose} disabled={loading}>
+          {t<string>('Forgot your password?')}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText className='mb-4'>
+            {t<string>(
+              `Coong Wallet cannot recover wallet password for you. If you have trouble unlocking your wallet, you will need to reset the wallet. Make sure you have your secret recovery phrase before proceed further.`,
+            )}
+          </DialogContentText>
+          <DialogContentText className='mb-4'>{t<string>(`Type "Reset wallet" to continue.`)}</DialogContentText>
+          <Box className='flex flex-col gap-4' component='form' autoComplete='off' onSubmit={doResetWallet}>
+            <TextField
+              label={t<string>('Reset wallet')}
+              type='text'
+              fullWidth
+              onChange={(e) => setConfirmation(e.target.value)}
+              value={confirmation}
+            />
+            <div className='flex gap-4 mt-4'>
+              <Button variant='text' onClick={handleClose} disabled={loading}>
+                {t<string>('Cancel')}
+              </Button>
+              <Button type='submit' fullWidth disabled={confirmation !== CONFIRMATION_WORDS || loading}>
+                {t<string>('Reset Wallet')}
+              </Button>
+            </div>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
