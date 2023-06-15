188f4565283c603853ccb2c0ef87e8168bbff144
Thang X. Vu <thang@coongwallet.io>
2023-06-15 17:35:18 +0700

    publish: v0.0.22
     - @coong/base@0.0.22
     - @coong/keyring@0.0.22
     - @coong/sdk@0.0.22
     - @coong/utils@0.0.22

--- packages/keyring/src/packageInfo.ts
+++ packages/keyring/src/packageInfo.ts
@@ -1,3 +1,3 @@
 // THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
 
-export const packageInfo = { name: '@coong/keyring', version: '0.0.22-alpha.0' };
+export const packageInfo = { name: '@coong/keyring', version: '0.0.22' };
