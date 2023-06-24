d9c50b41b4d2a1af256c1b18785d7e12d248e971
shouse <aowiej23s@gmail.com>
2023-06-24 16:33:58 +0700

    refactor: resize qrcode at small screen (#60)

--- packages/ui/src/hooks/useQrCodeSize.ts
+++ packages/ui/src/hooks/useQrCodeSize.ts
@@ -3,7 +3,7 @@ import { useMeasure } from 'react-use';
 export default function useQrCodeSize() {
   const [containerRef, { width }] = useMeasure<HTMLDivElement>();
 
-  const size = width > 300 ? 250 : width - 64;
+  const size = width > 300 ? 250 : width - 20;
 
   return { containerRef, size };
 }
